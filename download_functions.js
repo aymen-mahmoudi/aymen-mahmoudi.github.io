fd399f4c07fc24a1c3bf2e859a71dc3e6faf826f
aymen-mahmoudi <mahmoudi7050@gmail.com>
2025-03-06 20:52:10 +0100

    upd

--- download_functions.js
+++ download_functions.js
@@ -1,4 +1,6 @@
 var dict = {
+    link_cv_en: 'https://filedn.eu/lURhr7yoDog5QqszLEQkrtu/Personal_webpage/CVs/MAHMOUDI_EN.pdf',
+    link_cv_fr: 'https://filedn.eu/lURhr7yoDog5QqszLEQkrtu/Personal_webpage/CVs/MAHMOUDI_FR.pdf',
     link_rec_AO: 'https://filedn.eu/lURhr7yoDog5QqszLEQkrtu/Personal_webpage/Recommendations/Abdelkarim-Ouerghi.pdf',
     link_rec_VB:'https://filedn.eu/lURhr7yoDog5QqszLEQkrtu/Personal_webpage/Recommendations/Vincent-Bouchiat.pdf',
     link_rec_PT:'https://filedn.eu/lURhr7yoDog5QqszLEQkrtu/Personal_webpage/Recommendations/Pascal-Turban.pdf',
@@ -10,6 +12,34 @@ var dict = {
   
   
   
+// Function to open a link after password verification
+function openLinkWithPassword_cv_en() {
+    // Prompt the user for a password
+    var password = prompt("Please enter the password:");
+    
+    // Check if the password is correct
+    if (password === "paris75") {
+        // Replace 'https://example.com' with the link you want to open
+        window.open(dict.link_cv_en, '_blank');
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
+
+// Function to open a link after password verification
+function openLinkWithPassword_cv_fr() {
+    // Prompt the user for a password
+    var password = prompt("Please enter the password:");
+    
+    // Check if the password is correct
+    if (password === "paris75") {
+        // Replace 'https://example.com' with the link you want to open
+        window.open(dict.link_cv_fr, '_blank');
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
+
 // Function to open a link after password verification
 function openLinkWithPassword_rec_AO() {
     // Prompt the user for a password
